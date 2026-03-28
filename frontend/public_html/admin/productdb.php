6d7d7e69a2e44324c3ef345ad00625ae5127d4e9
Coursework592 <tomhowie.93@gmail.com>
2026-03-28 10:43:29 -0400

    Update productdb.php

--- frontend/public_html/admin/productdb.php
+++ frontend/public_html/admin/productdb.php
@@ -13,7 +13,31 @@
 
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Item Database | UWinByteStore</title>
+<meta name="description" content=" Learn about UWinByteStore and our technology catalog of recommended tech products and accessories.">
+<meta name="keywords" content="technology catalog, tech accessories, keyboards, webcams, projectors, electronics">
+<meta name="robots" content="index, follow">
+<link rel="stylesheet" href="../css/main.css">
+</head>
+<!-- As with all our pages, it is structured as one top-level container that contains at least
+ one div for the maincontent, with the possibility of adding additional divs above or below
+ for common elements like a nav bar, or page header and footer.-->
+<body>
+<header>
+<h1>UWinByteStore</h1>
+<nav>
+    <a href="../index.html">Home</a>
+    <a href="../catalog.php">Products</a>
+    <a href="../about.html">About</a>
+    <a href="../help.html">Help</a>
+    <a href="../login.html">Login</a>
+    <a href="../signup.html">Signup</a>
+</nav>
+</header>
 
 <!-- As with all our pages, it is structured as one top-level container that contains at least
  one div for the maincontent, with the possibility of adding additional divs above or below
@@ -35,7 +59,7 @@
                     option ('item') of a product group. Click on an item in the list below to open a form where
                     that item can be edited or deleted. See the associated wiki page for further details.
                 </p>
-                <a href="help/productdb-help.html">Help</a>
+                <a href="../Help/productdb.html">Help</a>
                 <hr>
                 
                     <!-- Display a mobile-friendly table with the details of each item record.
